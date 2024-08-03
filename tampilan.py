ed40b6c5a0cffd04a9f8145259d45014a62bfb13
Reggi Aryunadi <113322309+ReggiAr@users.noreply.github.com>
2024-08-03 22:59:19 +0700

    Update tampilan.py
    ditambahkan upseudo 
    done

--- tampilan.py
+++ tampilan.py
@@ -13,7 +13,7 @@ import os
 
 # -------- -------- GUI Libraries
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QProgressBar, QTextEdit, QDialog
-from PyQt5.QtWidgets import QHBoxLayout, QLineEdit, QPushButton, QComboBox, QSpinBox, QMessageBox, QSpacerItem, QSizePolicy
+from PyQt5.QtWidgets import QFileDialog, QHBoxLayout, QLineEdit, QPushButton, QComboBox, QSpinBox, QMessageBox, QSpacerItem, QSizePolicy
 from PyQt5.QtGui import QPixmap, QFont, QIcon,  QDesktopServices
 from PyQt5.QtCore import Qt, QUrl, pyqtSignal
 
@@ -22,7 +22,9 @@ from datetime import date
 import datetime as dt
 from astropy.time import Time
 
-
+# -------- -------- Time Libraries
+from openpyxl import Workbook
+import numpy as np
 
 # -------- GUI Design
 
@@ -44,7 +46,7 @@ class jendelautama(QWidget):
 
         judul = QLabel(self)
         judul.setText("  Time and Frequency Remote Calibration Software\t\t\t\t\n  CGGTTS Analyzer")
-        judul.setStyleSheet(f"{oren}; border-radius :15px; font-weight: DemiBold")
+        judul.setStyleSheet(f"{oren}; font-weight: DemiBold")
         judul.setFont(QFont("Inter", 12))
 
         spasi = QSpacerItem(60, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
@@ -255,6 +257,7 @@ class jendelautama(QWidget):
         self.uPsudorange.setText("uPsudorange")
         self.uPsudorange.setFont(font)
         self.uPsudorange.setStyleSheet(oren)
+        self.uPsudorange.clicked.connect(self.uPseudorange)
 
         spasi = QSpacerItem(60, 100, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
@@ -293,6 +296,209 @@ class jendelautama(QWidget):
         foot.addWidget(kaki)
 
         return foot
+
+    #---------------------- uPseudorange
+    #GUI
+    def uPseudorange (self):
+        uPseudoBox = QDialog(self)
+        uPseudoBox.setWindowTitle("uPseudorange Calculator")
+
+        judul = QLabel(uPseudoBox)
+        judul.setText(" uPseudorange Calculator\t")
+        judul.setFont(font)
+        judul.setStyleSheet(f"{oren}; font-weight: Bold")
+
+        tutor = QLabel(uPseudoBox)
+        tutor.setText("Prepare a folder containing CGGTTS data for a full month in .txt format, with files named from 1 to 30/31")
+        tutor.setFont(font)
+        tutor.setStyleSheet(oren)
+
+        cfolder = QPushButton(uPseudoBox)
+        cfolder.setText("Choose Folder")
+        cfolder.setFont(font)
+        cfolder.setStyleSheet(oren)
+        cfolder.clicked.connect(self.showDialog)
+
+        self.foldir = QLineEdit(uPseudoBox)
+        self.foldir.setFont(font)
+        self.foldir.setReadOnly(True)
+
+        jumlahlabel = QLabel(uPseudoBox)
+        jumlahlabel.setText("Total Days")
+        jumlahlabel.setFont(font)
+
+        self.jumlah = QComboBox(uPseudoBox)
+        self.jumlah.addItems(["28","29","30","31"])
+        self.jumlah.setFont(font)
+        self.jumlah.setStyleSheet(oren)
+
+        cal = QPushButton(uPseudoBox)
+        cal.setText("Calculate")
+        cal.setFont(font)
+        cal.setStyleSheet(oren)
+        cal.clicked.connect(self.calUPseudo)
+
+        self.PseudoOutput = QLineEdit(uPseudoBox)
+        self.PseudoOutput.setFont(font)
+        self.PseudoOutput.setReadOnly(True)
+
+        snumber = QLabel(uPseudoBox)
+        snumber.setText("Sampling Number")
+        snumber.setFont(font)
+
+        self.samplingnumber = QLineEdit(uPseudoBox)
+        self.samplingnumber.setFont(font)
+
+        o = QLabel(uPseudoBox)
+        o.setText("|")
+        o.setFont(font)
+
+        slabel = QLabel(uPseudoBox)
+        slabel.setText("STDEV")
+        slabel.setFont(font)
+
+        ulabel = QLabel(uPseudoBox)
+        ulabel.setText("uPseudorange")
+        ulabel.setFont(font)
+
+        self.uPs = QLineEdit(uPseudoBox)
+        self.uPs.setFont(font)
+        self.uPs.setReadOnly(True)
+
+        spasi = QSpacerItem(30, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        kaki = QLabel(uPseudoBox)
+        kaki.setText("CGGTTS Analyzer for Cesium Atomic Clock Remote Calibration © SNSU Time and Frequency Laboratory 2024 ")
+        kaki.setFont(font)
+
+        satu = QHBoxLayout()
+        satu.addWidget(cfolder)
+        satu.addWidget(self.foldir)
+        satu.addWidget(jumlahlabel)
+        satu.addWidget(self.jumlah)
+        satu.setStretchFactor(cfolder,1)
+        satu.setStretchFactor(self.foldir,5)
+        satu.setStretchFactor(jumlahlabel,1)
+        satu.setStretchFactor(self.jumlah,1)
+
+        dua = QHBoxLayout()
+        dua.addWidget(cal)
+        dua.addWidget(snumber)
+        dua.addWidget(self.samplingnumber)
+        dua.addWidget(o)
+        dua.addWidget(slabel)
+        dua.addWidget(self.PseudoOutput)
+        dua.addWidget(ulabel)
+        dua.addWidget(self.uPs)
+
+        layout = QVBoxLayout(uPseudoBox)
+        layout.addWidget(judul)
+        layout.addItem(spasi)
+        layout.addWidget(tutor)
+        layout.addItem(spasi)
+        layout.addLayout(satu)
+        layout.addLayout(dua)
+        layout.addItem(spasi)
+        layout.addWidget(kaki)
+
+        uPseudoBox.exec_()
+
+    #----------Buka File
+    def showDialog(self):
+        # Buka dialog pemilihan folder
+        folder = QFileDialog.getExistingDirectory(self, 'Pilih Folder')
+
+        # Jika pengguna memilih folder, tampilkan alamat folder di QLineEdit
+        if folder:
+            self.foldir.setText(folder)
+
+    # ----------- Perhitungan
+    def calUPseudo(self):
+        
+        days = self.jumlah.currentText()
+        day = int(days)
+        workbook = Workbook()
+        worksheet = workbook.create_sheet(title=f" Data CGGTTS ")
+
+        stdev = []
+        try:
+            for a in range (day):
+                refgps = []
+                self.PseudoOutput.setText(f"Calculate {a+1} day")
+                #Read File
+                with open(f'{self.foldir.text()}/{a+1}.txt', 'r') as Filestd:
+                    # Baca semua baris kecuali baris pertama
+                    cggtts = Filestd.readlines()
+
+                nama_kolom = ['SAT','STTIME','REFSYS']
+                header = cggtts[17].strip().split()
+                std_indeks_kolom = [header.index(kolom) for kolom in nama_kolom]
+
+                # Loop melalui setiap baris file
+                for line in cggtts[19:]:  # Mulai dari baris kedelapan belas karena baris pertama adalah header
+                    data = line.strip().split()
+        
+                    # Pastikan jumlah elemen dalam baris sesuai dengan jumlah kolom yang diharapkan
+                    if len(data) >= max(std_indeks_kolom) + 1:
+
+                        # Masukkan ke dalam list yang sesuai
+                        if data[std_indeks_kolom[2]].isdigit() or data[std_indeks_kolom[2]].startswith("-") or data[std_indeks_kolom[2]].startswith("+"):
+                            refgps.append(data[std_indeks_kolom[2]])
+                        else:
+                            print("Skipping stdRefGPS non-numeric values\n")
+                #print(refgps)
+                refgps_array = np.array(refgps)
+                refgps_array = refgps_array.astype(int)
+
+                mean = np.mean(refgps_array)
+                stdev.append(mean)
+                self.PseudoOutput.setText(f"Average {a+1} day is : {mean}\n")
+
+                #print ("Transfering STD data to Excel File . . .\n")
+                cell = worksheet.cell(row=2,column=2+a)
+                cell.value = a+1
+
+                for i, item in enumerate(refgps, start=3):
+                    cell = worksheet.cell(row=i, column=2+a)
+                    cell.value = item
+
+            stdev = np.array(stdev)
+            standar_deviasi = np.std(stdev)
+            hasil = standar_deviasi/10
+            hasil=f"{hasil:.2f} ns"
+            self.PseudoOutput.setText(str(hasil))
+
+            akardua = np.sqrt(2)
+
+            sum = workbook.create_sheet(title=f" Data CGGTTS ")
+
+            cell = sum.cell (row=2, column=2)
+            cell.value = "Rata-Rata Setiap Bulan"
+
+            cell = sum.cell (row=2, column=4)
+            cell.value = "Standar Deviasi"
+            cell = sum.cell (row=2, column=5)
+            cell.value = standar_deviasi
+
+            for i, item in enumerate(stdev, start=3):
+                cell = sum.cell(row=i, column=2+a)
+                cell.value = item
+
+            namafile = f"uPseudorange.xlsx"
+            workbook.save(namafile)
+
+        except:
+            self.PseudoOutput.setText("Please Check Folder Content")
+
+
+        try:
+            samp = float(self.samplingnumber.text())
+            sd = float(standar_deviasi/10)
+            ya = sd/np.sqrt(samp)
+            ups = ya*akardua
+            self.uPs.setText(f"{ups:2f} ns")
+        except:
+            self.uPs.setText("Please input Sampling Number")
+
 
 
     # semuanya
